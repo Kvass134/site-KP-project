ff6fbfc8aafc2234fcccee161ad978353508b676
Kvass134 <102312883+Kvass134@users.noreply.github.com>
2022-09-09 15:25:32 +0400

    Beta FIles

---
+++ site/account.js
@@ -0,0 +1,10 @@
+var content;
+var name;
+if (content == "ok") {
+content = localStorage.getItem("content");
+name = localStorage.getItem("name");
+document.write ("Привет" + name + "!");
+}else {
+alert
+//setItem
+}
